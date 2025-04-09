e61b6307ec1f7421061234a548b30420f2b1cfe5
natsime <natsimeone2@gmail.com>
2025-04-09 19:11:24 +0200

    added API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Sempre caro mi fu quest'ermo colle",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "74a34f8043d8o722fcb65ftc400b35d7";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4-line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning and remove any Three backquotes.";
+  let prompt = `User instructions: Generate an Italian poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem..");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-genetator-form");
 poemFormElement.addEventListener("submit", generatePoem);
